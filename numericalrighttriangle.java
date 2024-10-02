38dd0b5ec27bfd66d8ff16fac81238c3f866bea4
Manish1775 <vermamanish1775@gmail.com>
2024-10-02 15:46:37 +0530

    first commit

---
+++ numericalrighttriangle.java
@@ -0,0 +1,20 @@
+package newbi;
+
+public class numericalrighttriangle {
+
+	public static void main(String[] args) {
+		
+		int n=0;
+		for(int i=0;i<=5;i++) {
+			for(int j=1;j<i;j++) {
+				System.out.print(" "+n);
+				n++;
+			}
+		
+			System.out.println(" ");
+	
+		}
+		
+		
+	}
+}
